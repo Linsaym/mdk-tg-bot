b36a902ed162ac5c78ade5b1fe520d40a4fe29ae
Linsaym <leva397@mail.ru>
2025-09-23 14:57:54 +0300

    rofl

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1828,7 +1828,7 @@
             </ul>
             <ul class="flex gap-3 text-sm leading-normal">
                 <li>
-                    <button onclick="window.close()"
+                    <button onclick="alert('Мы такое не умеем :(')"
                             class="inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal">
                         Закрыть бесполезную вкладку :D
                     </button>
