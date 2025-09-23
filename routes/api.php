6c1da936f7f2be12ad46f08be80c35fdef4f0d25
Linsaym <leva397@mail.ru>
2025-09-23 18:43:04 +0300

    fix

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\TestTelegramBotController;
 Route::get('/telegram-webhook', fn () => 123);
 
 Route::post('/telegram-webhook', [TelegramBotController::class, 'handleWebhook']);
+Route::post('/telegram-webhook-2', [TelegramBotController::class, 'handleWebhook']);
 
 //Для тестового телеграм бота
 Route::post('/test-telegram-webhook', [TestTelegramBotController::class, 'handleWebhook']);
